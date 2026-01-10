d2507b38c9646951aecbdbac5fbce521164035e2
Lakshmi Devi <24881A0528@student.vardhaman.org>
2026-01-10 20:27:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,12 +12,15 @@ function addMessage(text, sender) {
 
 function typingEffect(text, callback) {
   let i = 0;
+  const msg = document.createElement("div");
+  msg.className = "bot-msg";
+  chatBox.appendChild(msg);
+
   const interval = setInterval(() => {
-    if (i < text.length) {
-      if (i === 0) addMessage("", "bot");
-      chatBox.lastChild.innerText += text[i];
-      i++;
-    } else {
+    msg.innerText += text[i];
+    i++;
+    chatBox.scrollTop = chatBox.scrollHeight;
+    if (i >= text.length) {
       clearInterval(interval);
       if (callback) callback();
     }
@@ -37,63 +40,73 @@ function showOptions(options) {
 function handleUserChoice(choice) {
   addMessage(choice, "user");
   optionsDiv.innerHTML = "";
-
-  setTimeout(() => {
-    chatbotReply(choice.toLowerCase());
-  }, 500);
+  setTimeout(() => chatbotReply(choice.toLowerCase()), 400);
 }
 
 function chatbotReply(input) {
   if (input === "courses") {
-    typingEffect("Great choice 😊 Which branch are you interested in?", () => {
-      showOptions(["CSE", "ECE", "EEE", "MECH", "CIVIL"]);
-    });
+    typingEffect(
+      "Great choice 😊\nWhich branch are you interested in?",
+      () => showOptions(["CSE", "ECE", "EEE", "MECH", "CIVIL"])
+    );
   }
+
   else if (input === "cse") {
-    typingEffect("CSE is one of our most popular branches 💻", () => {
-      typingEffect("What would you like to know about CSE?", () => {
-        showOptions(["Subjects", "Placements", "Fees", "Back"]);
-      });
-    });
+    typingEffect(
+      "CSE is one of our most popular branches 💻\nWhat would you like to know?",
+      () => showOptions(["Subjects", "Placements", "Fees", "Back"])
+    );
   }
+
   else if (input === "subjects") {
-    typingEffect("CSE includes Programming, Data Structures, AI, and Web Technologies.", () => {
-      askAnythingElse();
-    });
+    typingEffect(
+      "CSE includes Programming, Data Structures, AI and Web Technologies.",
+      askMore
+    );
   }
+
   else if (input === "placements") {
-    typingEffect("Our college offers excellent placement support with top recruiters.", () => {
-      askAnythingElse();
-    });
+    typingEffect(
+      "Our college provides excellent placement support with top recruiters.",
+      askMore
+    );
   }
+
   else if (input === "fees") {
-    typingEffect("Fee details vary per course. Please check the official website.", () => {
-      askAnythingElse();
-    });
+    typingEffect(
+      "Fee details vary per course.\nPlease check the official college website.",
+      askMore
+    );
   }
+
   else if (input === "timings") {
-    typingEffect("College timings are from 9:00 AM to 4:30 PM 🕘", () => {
-      askAnythingElse();
-    });
+    typingEffect(
+      "College timings are from 9:00 AM to 4:30 PM 🕘",
+      askMore
+    );
   }
+
   else if (input === "facilities") {
-    typingEffect("We provide library, hostel, labs, and sports facilities 🏫", () => {
-      askAnythingElse();
-    });
+    typingEffect(
+      "Facilities include library, hostel, labs and sports 🏫",
+      askMore
+    );
   }
+
   else if (input === "back") {
     startBot();
   }
+
   else {
-    typingEffect("Sorry 😅 I didn’t understand that. Please choose an option.");
-    showMainMenu();
+    typingEffect(
+      "Sorry 😅 I didn’t understand that.\nPlease choose an option.",
+      showMainMenu
+    );
   }
 }
 
-function askAnythingElse() {
-  typingEffect("What else would you like to know?", () => {
-    showMainMenu();
-  });
+function askMore() {
+  typingEffect("What else would you like to know?", showMainMenu);
 }
 
 function showMainMenu() {
@@ -101,11 +114,14 @@ function showMainMenu() {
 }
 
 function startBot() {
-  typingEffect("👋 Welcome to Vardhaman College Enquiry Bot 🤖", () => {
-    typingEffect("Please select an option below 👇", () => {
-      showMainMenu();
-    });
-  });
+  chatBox.innerHTML = "";
+  typingEffect(
+    "👋 Welcome to Vardhaman College Enquiry Bot 🤖",
+    () => typingEffect(
+      "Please select an option below 👇",
+      showMainMenu
+    )
+  );
 }
 
 function sendUserMessage() {
@@ -113,7 +129,7 @@ function sendUserMessage() {
   if (!text) return;
   addMessage(text, "user");
   userInput.value = "";
-  setTimeout(() => chatbotReply(text.toLowerCase()), 500);
+  setTimeout(() => chatbotReply(text.toLowerCase()), 400);
 }
 
 startBot();
